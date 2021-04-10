71b77a9baeffc18bf3156b9cf97d21fef4b1ab28
Tyler Schiewe <tyler.schiewe@gmail.com>
2021-04-10 14:43:23 -0700

    add another example

---
+++ examples/counter/src/main/java/com/tylerschiewe/fxibit/examples/counter/App.java
@@ -0,0 +1,40 @@
+/*
+ * Fxibit
+ *
+ * Copyright © 2021 Tyler Schiewe
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.tylerschiewe.fxibit.examples.counter;
+
+import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Counter");
+        StackPane stackPane = new StackPane();
+        SimpleIntegerProperty count = new SimpleIntegerProperty(0);
+        Button counterButton = new Button();
+        counterButton.textProperty().bind(Bindings.createStringBinding(() ->
+                count.getValue().toString(), count)
+        );
+        counterButton.setOnAction(e -> count.setValue(count.getValue() + 1));
+        stackPane.getChildren().add(counterButton);
+        Scene scene = new Scene(stackPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
